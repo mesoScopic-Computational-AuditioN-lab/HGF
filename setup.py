4ad0cfabe2f35b4336863672edad77327c23ace8
Jorie van Haren <105725985+jorievanharen@users.noreply.github.com>
2022-06-10 14:10:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,13 @@ from setuptools import setup
 VERSION = '0.1.0'
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='HGF',
     url='https://github.com/mesoScopic-Computational-AuditioN-lab/HGF',
     author='Jorie van Haren',
     author_email='jjg.vanharen@maastrichtuniversity.nl',
-    # Needed to actually package something
     packages=['HGF'],
-    # Needed for dependencies
     install_requires=['numpy'],
-    # *strongly* suggested for sharing
     version=VERSION,
-    # The license can be anything you like
     license='MIT',
     description='A Hierarchical Gaussian Filter Toolbox for Python',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
 )
